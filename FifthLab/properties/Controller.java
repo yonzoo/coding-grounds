186a7efd30132f4ccbb74bd441dd3c9c49ecb257
iamnzrv <nzrvofficial@gmail.com>
2019-10-21 14:23:41 +0300

    Init lab 5

---
+++ FifthLab/properties/Controller.java
@@ -0,0 +1,8 @@
+package properties;
+
+public class Controller {
+    public static void main(String[] args) {
+        PropertiesReader.readProps();
+        PropertiesReader.printProps();
+    }
+}
